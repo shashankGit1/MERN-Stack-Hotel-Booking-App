cd488180b2d244f71a98b333d1a925ce8d2339cd
Shashank <1806421@kiit.ac.in>
2022-06-03 17:13:39 +0530

    feature-auth-1

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Hotel from "./pages/hotel/Hotel";
 import List from "./pages/list/List";
-
+import Login from "./login/Login";
 function App() {
   return (
     <BrowserRouter>
@@ -11,6 +11,7 @@ function App() {
         <Route path="/hotels" element={<List/>} />
         <Route path="/hotels/:id" element={<Hotel/>} />
         <Route path="/hotel" element={<Hotel/>} />
+        <Route path="/login" element={<Login/>} />
       </Routes>
     </BrowserRouter>
   );
